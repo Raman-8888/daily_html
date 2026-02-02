f525d3fe177a05a44c2aab0219e947cef3b5a7f7
Raman Negi <149420966+Raman-8888@users.noreply.github.com>
2026-02-02 23:29:11 +0530

    Time: 1 ms (99.21%), Space: 45.2 MB (88.1%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -3,14 +3,16 @@ class Solution {
         int min1 = Integer.MAX_VALUE;
         int min2 = Integer.MAX_VALUE;
 
-        for (int i = 1; i < nums.length; i++) {
-            int x = nums[i];
-
-            if (x < min1) {
-                min2 = min1;
-                min1 = x;
-            } else if (x < min2) {
-                min2 = x;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]<min1)
+            {
+                min2=min1;
+                min1=nums[i];
+            }
+            else if(nums[i]<min2)
+            {
+                min2=nums[i];
             }
         }
 
